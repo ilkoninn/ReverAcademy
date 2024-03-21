507737ffd8c3bf0983e065055f25dc4b9c1ac6d7
Recebov Ilkin <recebovilkin256@gmail.com>
2024-03-21 15:07:00 +0400

    Polindrom task has been updated

---
+++ Tutorial Task/Program.cs
@@ -0,0 +1,32 @@
+﻿// Polindrom Task
+
+namespace Tutorial_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1211, digit, sum = 0;
+            int temp = number;
+
+            while (number > 0)
+            {
+                digit = number % 10; 
+
+                sum = sum * 10 + digit;
+
+                number = number / 10; 
+            }
+
+            if(temp == sum)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+        }
+    }
+}
